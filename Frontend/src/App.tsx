35391d3a0681dfb5113437a4f778d5da2d6402db
AnUbHaVafs <annubhav2@gmail.com>
2024-01-11 13:28:43 +0530

    google signin changes

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ChatPage from "./Pages/ChatPage";
-import { jwtDecode } from "jwt-decode";
-import { useEffect } from "react";
-import { handleUserLogin } from "./helpers/helpers";
+// import { jwtDecode } from "jwt-decode";
+// import { useEffect } from "react";
+// import { handleUserLogin } from "./helpers/helpers";
 import "./App.css";
 
 declare global {
@@ -12,35 +12,35 @@ declare global {
 }
 
 function App() {
-  const navigate = useNavigate();
-  const handleCallbackResponse = async (res: any) => {
-    let encodedPayload: string = "";
-    Object.keys(res).map((key) => {
-      if (key === "credential") {
-        encodedPayload = res[key];
-      }
-    });
-    const token = encodedPayload;
-    const decoded = jwtDecode(token);
-    const resp = await handleUserLogin(decoded);
-    if (resp) {
-      navigate("/chat");
-    }
-  };
+  // const navigate = useNavigate();
+  // const handleCallbackResponse = async (res: any) => {
+  //   let encodedPayload: string = "";
+  //   Object.keys(res).map((key) => {
+  //     if (key === "credential") {
+  //       encodedPayload = res[key];
+  //     }
+  //   });
+  //   const token = encodedPayload;
+  //   const decoded = jwtDecode(token);
+  //   const resp = await handleUserLogin(decoded);
+  //   if (resp) {
+  //     navigate("/chat");
+  //   }
+  // };
 
-  useEffect(() => {
-    /* global google */
-    google.accounts.id.initialize({
-      client_id:
-        "769870810839-bl5tchvlpq7bov7crs26da9qb6i4fbkk.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
-    google.accounts.id.renderButton(document.getElementById("signInDiv"), {
-      theme: "outline",
-      size: "large",
-    });
-    // eslint-disable-next-line
-  }, []);
+  // useEffect(() => {
+  //   /* global google */
+  //   google.accounts.id.initialize({
+  //     client_id:
+  //       "769870810839-bl5tchvlpq7bov7crs26da9qb6i4fbkk.apps.googleusercontent.com",
+  //     callback: handleCallbackResponse,
+  //   });
+  //   google.accounts.id.renderButton(document.getElementById("signInDiv"), {
+  //     theme: "outline",
+  //     size: "large",
+  //   });
+  //   // eslint-disable-next-line
+  // }, []);
 
   return (
     <div className="App">
