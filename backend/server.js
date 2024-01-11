3b565752b4ed827b79b8865462b2527cb6e31c80
AnUbHaVafs <annubhav2@gmail.com>
2024-01-11 13:51:44 +0530

    cors error fixed: Frontend Deployed

--- backend/server.js
+++ backend/server.js
@@ -114,7 +114,8 @@ const server = app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:5173",
+    origin:
+      "https://world-web-flow-smoothing-your-chat-experience-q3ti.vercel.app",
     // credentials: true,
   },
 });
